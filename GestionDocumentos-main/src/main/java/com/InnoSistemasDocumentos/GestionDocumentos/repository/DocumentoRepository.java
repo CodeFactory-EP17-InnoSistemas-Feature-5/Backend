08b44c2b9b57c7b70104802a1af6234673c6bb86
Satshman <santiago.morales1000@gmail.com>
2025-06-14 17:41:19 -0500

    Delete Old GestionDocumentos-main directory

--- GestionDocumentos-main/src/main/java/com/InnoSistemasDocumentos/GestionDocumentos/repository/DocumentoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.InnoSistemasDocumentos.GestionDocumentos.repository;
-
-import com.InnoSistemasDocumentos.GestionDocumentos.model.Documento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocumentoRepository extends JpaRepository<Documento, Long> {
-}
